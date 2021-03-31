c288d9d5f1e83135fd43673b874d643b6b5bacb6
= <44462080+diksha1009@users.noreply.github.com>
2021-03-31 13:09:24 +0530

    css changes

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 const Card = ({cardData}) => {
+    // console.log(cardData.id);
     return (
-        <section className="card" style={{ borderTop: `3px solid ${cardData.borderColor}` }}>
+        <div className={"card item" + cardData.id} style={{ borderTop: `5px solid ${cardData.borderColor}` }}>
             <h2>{cardData.title}</h2>
 				<p>{cardData.desc}</p>
 				<div>
 					<img src={cardData.iconSrc} alt="..." className="icon" />
 				</div>
-        </section>
+        </div>
     )
 }
 
--- src/Component/Heading1.js
+++ src/Component/Heading1.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Heading1 = () => {
     return (
         <div>
-            <h1> Reliable, efficient delivery </h1>
-            <h1>Powered by Technology</h1>
-            <p>Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+            <h1 className="heading title"> Reliable, efficient delivery </h1>
+            <h1 className="heading author">Powered by Technology</h1>
+            <p className="description">Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
         </div>
     )
 }
